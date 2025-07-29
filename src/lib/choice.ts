9000c5b9d5bb822963cbdc222b051318d9520f8a
Hiroshi Hagiwara <mukakinkatsubou@gmail.com>
2025-07-29 22:30:39 +0900

    fix:
    途中

--- src/lib/choice.ts
+++ src/lib/choice.ts
@@ -4,184 +4,159 @@ import { WEAPON as WEAPON_V2 } from '@const/v2/weapons';
 import type { Weapon, Config, ConfigAdapter } from '@common/types';
 import { chooseRandomly, shuffle, assertUndefined } from '@common/functions';
 import type { Result } from 'neverthrow';
-import { ServerConfigAdapter } from '@adapters/server';
 
-let currentConfig: Config | null = null;
+class WeaponEntity {
+    private currentConfig: Config;
 
-const setWeaponContext = (adapter: ConfigAdapter): void => {
-    currentConfig = adapter.getConfig();
-};
-
-const getWeaponContext = (): Config => {
-    if (!currentConfig) {
-        currentConfig = new ServerConfigAdapter().getConfig();
-    }
-    return currentConfig;
-};
-
-const getWeapons = (): readonly Weapon[] => {
-    const config = getWeaponContext();
-    switch (config.gameVersion) {
-        case '2':
-            return WEAPON_V2;
-        case '3':
-            return WEAPON_V3;
-        default:
-            return WEAPON_V3;
-    }
-};
-
-// ブラックリストに登録されているものを省く
-const getBlFilteredWeapon = (): Weapon[] => {
-    const config = getWeaponContext();
-    const weapons = getWeapons();
-    return weapons.filter(v => !config.weaponBlacklist.includes(v.name))
-                 .filter(v => !config.weaponSmallCategoryBlacklist.includes(v.sc))
-                 .filter(v => !config.weaponLargeCategoryBlacklist.includes(v.lc));
-};
-
-const getRandomWeapon = (): Weapon => {
-    // WEAPON の中からランダムに1つ選ぶ
-    const weapons = getBlFilteredWeapon();
-    const weapon = weapons[Math.floor(Math.random() * weapons.length)];
-
-    assertUndefined(weapon);
-
-    return weapon;
-};
-
-const getRandomWeaponPair = (): [Weapon, Weapon] => {
-    const config = getWeaponContext();
-    // セーフティモードでない = 編成事故防止ロジックを適用しない
-    if (!config.safetyMode) {
-        return [getRandomWeapon(), getRandomWeapon()];
+    constructor (adapter: ConfigAdapter) {
+        this.currentConfig = adapter.getConfig();
     }
 
-    // SHORT の武器をランダムに1つ取得
-    const weapons = getBlFilteredWeapon();
-    const shortRangeWeapons = weapons.filter(w => w.range === 'SHORT');
-
-    if (!shortRangeWeapons.length) {
-        return [getFreeWeapon(), getFreeWeapon()];
+    private getWeapons (): readonly Weapon[] {
+        switch (this.currentConfig.gameVersion) {
+            case '2':
+                return WEAPON_V2;
+            case '3':
+                return WEAPON_V3;
+            default:
+                return WEAPON_V3;
+        }
     }
 
-    const shortWeapon = chooseRandomly(shortRangeWeapons);
-
-    // `range` が MID または LONG の武器を抽出
-    // どちらかが KILL であるが、両方とも KILL にならないようにする
-    const midLongWeapons = weapons.filter(v => v.range === 'MID' || v.range === 'LONG');
-    const midLongFiltered = midLongWeapons.filter(v => shortWeapon.role !== 'KILL' ? v.role === 'KILL' : v);
-
-    // シューター以外のカテゴリ被りが発生しないように
-    const midLongLcFiltered = midLongFiltered.filter(v => v.lc === 'SHOOTER' || v.lc !== shortWeapon.lc);
-    
-    if (!midLongLcFiltered.length) {
-        return [shortWeapon, getFreeWeapon()];
-    }
-    
-    const secondWeapon = chooseRandomly(midLongLcFiltered);
-
-    return [shortWeapon, secondWeapon];
-};
-
-const getRandomWeaponTrio = (): [Weapon, Weapon, Weapon] => {
-    const config = getWeaponContext();
-    // セーフティモードでない = 編成事故防止ロジックを適用しない
-    if (!config.safetyMode) {
-        return [getRandomWeapon(), getRandomWeapon(), getRandomWeapon()];
+    private getBlFilteredWeapon (): Weapon[] {
+        const weapons = this.getWeapons();
+        return weapons.filter(v => !this.currentConfig.weaponBlacklist.includes(v.name))
+                     .filter(v => !this.currentConfig.weaponSmallCategoryBlacklist.includes(v.sc))
+                     .filter(v => !this.currentConfig.weaponLargeCategoryBlacklist.includes(v.lc));
     }
 
-    const weaponPair = getRandomWeaponPair();
+    private getRandomWeapon (): Weapon {
+        const weapons = this.getBlFilteredWeapon();
+        const weapon = weapons[Math.floor(Math.random() * weapons.length)];
 
-    // 持っていない射程
-    const weapons = getBlFilteredWeapon();
-    const ranges = weaponPair.map(v => v.range);
-    const filterRange = weapons.filter(v => !ranges.includes(v.range));
+        assertUndefined(weapon);
 
-    // 持っていない役割 塗り優先
-    const roles = weaponPair.map(v => v.role);
-    const paintSelected = weaponPair.some(v => v.role === 'PAINT');
-    const filterRangeRole = filterRange.filter(v => !paintSelected ? v.role === 'PAINT' : !roles.includes(v.role));
-
-    // シューター以外のカテゴリ被りが発生しないように
-    const largeCategories = weaponPair.map(v => v.lc);
-    const filterRangeRoleLc = filterRangeRole.filter(v => v.lc === 'SHOOTER' || !largeCategories.includes(v.lc));
-
-    if (!filterRangeRoleLc.length) {
-        return [...weaponPair, getFreeWeapon()];
+        return weapon;
     }
 
-    const thirdWeapon = chooseRandomly(filterRangeRoleLc);
+    private getRandomWeaponPair (): [Weapon, Weapon] {
+        if (!this.currentConfig.safetyMode) {
+            return [this.getRandomWeapon(), this.getRandomWeapon()];
+        }
 
-    return [...weaponPair, thirdWeapon];
-};
+        const weapons = this.getBlFilteredWeapon();
+        const shortRangeWeapons = weapons.filter(w => w.range === 'SHORT');
 
-const getRandomWeaponTeam = (): [Weapon, Weapon, Weapon, Weapon] => {
-    const config = getWeaponContext();
-    // セーフティモードでない = 編成事故防止ロジックを適用しない
-    if (!config.safetyMode) {
-        return [getRandomWeapon(), getRandomWeapon(), getRandomWeapon(), getRandomWeapon()];
+        if (!shortRangeWeapons.length) {
+            return [this.getFreeWeapon(), this.getFreeWeapon()];
+        }
+
+        const shortWeapon = chooseRandomly(shortRangeWeapons);
+
+        const midLongWeapons = weapons.filter(v => v.range === 'MID' || v.range === 'LONG');
+        const midLongFiltered = midLongWeapons.filter(v => shortWeapon.role !== 'KILL' ? v.role === 'KILL' : v);
+
+        const midLongLcFiltered = midLongFiltered.filter(v => v.lc === 'SHOOTER' || v.lc !== shortWeapon.lc);
+        
+        if (!midLongLcFiltered.length) {
+            return [shortWeapon, this.getFreeWeapon()];
+        }
+        
+        const secondWeapon = chooseRandomly(midLongLcFiltered);
+
+        return [shortWeapon, secondWeapon];
     }
 
-    const weaponTrio = getRandomWeaponTrio();
+    private getRandomWeaponTrio (): [Weapon, Weapon, Weapon] {
+        if (!this.currentConfig.safetyMode) {
+            return [this.getRandomWeapon(), this.getRandomWeapon(), this.getRandomWeapon()];
+        }
 
-    // 2人目の短射程
-    const weapons = getBlFilteredWeapon();
-    const filterRange = weapons.filter(v => v.range === 'SHORT');
+        const weaponPair = this.getRandomWeaponPair();
 
-    // シューター以外のカテゴリ被りが発生しないように
-    const largeCategories = weaponTrio.map(v => v.lc);
-    const filterRangeLc = filterRange.filter(v => v.lc === 'SHOOTER' || !largeCategories.includes(v.lc));
+        const weapons = this.getBlFilteredWeapon();
+        const ranges = weaponPair.map(v => v.range);
+        const filterRange = weapons.filter(v => !ranges.includes(v.range));
 
-    // スモールカテゴリでの被りがないようにする
-    // 例えばボールドマーカーとボールドマーカーネオなど
-    const smallCategories = weaponTrio.map(v => v.sc);
-    const filterRangeLcSc = filterRangeLc.filter(v => !smallCategories.includes(v.sc));
+        const roles = weaponPair.map(v => v.role);
+        const paintSelected = weaponPair.some(v => v.role === 'PAINT');
+        const filterRangeRole = filterRange.filter(v => !paintSelected ? v.role === 'PAINT' : !roles.includes(v.role));
 
-    // ヘイト役が2枚編成にならないように
-    const tankSelected = weaponTrio.some(v => v.role === 'TANK');
-    const filterRangeLcScRole = filterRangeLcSc.filter(v => tankSelected ? v.role !== 'TANK' : v);
-    const lastWeapon = chooseRandomly(filterRangeLcScRole);
+        const largeCategories = weaponPair.map(v => v.lc);
+        const filterRangeRoleLc = filterRangeRole.filter(v => v.lc === 'SHOOTER' || !largeCategories.includes(v.lc));
 
-    if (!filterRangeLcScRole.length) {
-        return [...weaponTrio, getFreeWeapon()];
+        if (!filterRangeRoleLc.length) {
+            return [...weaponPair, this.getFreeWeapon()];
+        }
+
+        const thirdWeapon = chooseRandomly(filterRangeRoleLc);
+
+        return [...weaponPair, thirdWeapon];
     }
 
-    return [...weaponTrio, lastWeapon];
-};
+    private getRandomWeaponTeam (): [Weapon, Weapon, Weapon, Weapon] {
+        if (!this.currentConfig.safetyMode) {
+            return [this.getRandomWeapon(), this.getRandomWeapon(), this.getRandomWeapon(), this.getRandomWeapon()];
+        }
 
-const getFreeWeapon = (): Weapon => {
-    const weapons = getWeapons();
-    const free = weapons.find(v => v.role === 'FREE');
+        const weaponTrio = this.getRandomWeaponTrio();
 
-    assertUndefined(free);
+        const weapons = this.getBlFilteredWeapon();
+        const filterRange = weapons.filter(v => v.range === 'SHORT');
 
-    return free;
-};
+        const largeCategories = weaponTrio.map(v => v.lc);
+        const filterRangeLc = filterRange.filter(v => v.lc === 'SHOOTER' || !largeCategories.includes(v.lc));
+
+        const smallCategories = weaponTrio.map(v => v.sc);
+        const filterRangeLcSc = filterRangeLc.filter(v => !smallCategories.includes(v.sc));
+
+        const tankSelected = weaponTrio.some(v => v.role === 'TANK');
+        const filterRangeLcScRole = filterRangeLcSc.filter(v => tankSelected ? v.role !== 'TANK' : v);
+        const lastWeapon = chooseRandomly(filterRangeLcScRole);
+
+        if (!filterRangeLcScRole.length) {
+            return [...weaponTrio, this.getFreeWeapon()];
+        }
+
+        return [...weaponTrio, lastWeapon];
+    }
+
+    private getFreeWeapon (): Weapon {
+        const weapons = this.getWeapons();
+        const free = weapons.find(v => v.role === 'FREE');
+
+        assertUndefined(free);
+
+        return free;
+    }
+
+    public getWeaponsByNumber (): Result<Weapon[], Error> {
+        let weapons: Weapon[];
+        switch (this.currentConfig.playerNumber) {
+            case 1:
+                weapons = [this.getRandomWeapon()];
+                break;
+            case 2:
+                weapons = this.getRandomWeaponPair(); 
+                break;
+            case 3:
+                weapons = this.getRandomWeaponTrio();
+                break; 
+            case 4:
+                weapons = this.getRandomWeaponTeam();
+                break;
+            default:
+                return err(new Error('無効な番号です。1～4の値を指定してください。'));
+        }
+
+        const shuffled = shuffle(weapons);
+        return ok(shuffled);
+    }
+}
 
 export const getWeaponsByNumber = (adapter: ConfigAdapter): Result<Weapon[], Error> => {
-    setWeaponContext(adapter);
-    
-    let weapons: Weapon[];
-    switch (currentConfig?.playerNumber) {
-        case 1:
-            weapons = [getRandomWeapon()];
-            break;
-        case 2:
-            weapons = getRandomWeaponPair(); 
-            break;
-        case 3:
-            weapons = getRandomWeaponTrio();
-            break; 
-        case 4:
-            weapons = getRandomWeaponTeam();
-            break;
-        default:
-            return err(new Error('無効な番号です。1～4の値を指定してください。'));
-    }
-
-    const shuffled = shuffle(weapons);
-    return ok(shuffled);
+    const weaponEntity = new WeaponEntity(adapter);
+    return weaponEntity.getWeaponsByNumber();
 };
 
 
